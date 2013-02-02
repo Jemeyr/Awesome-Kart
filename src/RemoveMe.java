3c4d8f4c240b9a25635628b1d5b2ebd1049afe64
DDB- <nyrrules@gmail.com>
2013-02-01 18:09:25 -0800

    fixed over 9000

--- src/RemoveMe.java
+++ src/RemoveMe.java
@@ -5,7 +5,7 @@ public class RemoveMe {
 			boolean Conti = true;
 			int elec360power = 0;
 			while(Conti && elec360power <= 9000){
-				System.out.println(String.format("Conti's power is at %d", elec360power++));
+				System.out.println(String.format("Conti's power is at %d", ++elec360power));
 			}
 			System.out.println("CONTI'S ELEC 360 POWER LEVEL IS OVER 9000!!!!!!!!!!!");
 		}
